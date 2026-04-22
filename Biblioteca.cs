0dccdd921b3e640df30c9f8e46d94216e4361c0e
MatheuxP <matheuspupiaa@gmail.com>
2026-04-21 22:48:29 -0300

    novas funcoes bibliotecas

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -2,8 +2,64 @@ using biblioteca_tes2026.Models;
 
 class Biblioteca
 {
+    public static void CadastrarUsuario(List<Usuario> usuarios)
+    {
+        Console.Write("Digite o nome do usuário: ");
+        string nome = Console.ReadLine();
 
-     public static void CadastrarLivro(List<Livro> livros)
+        Console.Write("Digite a idade do usuário: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o telefone do usuário: ");
+        string telefone = Console.ReadLine();
+
+        usuarios.Add(new Usuario(nome, idade, telefone));
+        Console.WriteLine("Usuário cadastrado com sucesso!");
+    }
+
+    public static void ListarUsuarios(List<Usuario> usuarios)
+    {
+        Console.WriteLine("\nLista de Usuários:");
+        foreach (Usuario usuario in usuarios)
+        {
+            usuario.Exibir();
+        }
+    }
+
+ public static void ListarLivros(List<Livro> livros)
+    {
+        Console.WriteLine("\nLista de Livros:");
+        foreach (Livro livro in livros)
+        {
+            livro.Exibir();
+        }
+    }
+
+    public static void BuscarLivro(List<Livro> livros)
+{
+    Console.WriteLine("Digite o título do livro que deseja buscar: ");
+    string tituloBusca = Console.ReadLine();
+
+    bool livroEncontrado = false;
+
+    foreach (Livro li in livros)
+    {
+        if(li.Titulo.Equals(tituloBusca, StringComparison.OrdinalIgnoreCase))
+        {
+            li.Exibir();
+            livroEncontrado = true;
+            break;
+        }
+    }
+
+    if(livroEncontrado == false)
+    {
+        Console.WriteLine("Livro não encontrado.");
+    }
+}
+
+    
+    public static void CadastrarLivro(List<Livro> livros)
     {
         Console.Write("Digite o título do livro: ");
         string titulo = Console.ReadLine();
@@ -16,5 +72,112 @@ class Biblioteca
 
         livros.Add(new Livro(titulo, autor, ano));
         Console.WriteLine("Livro cadastrado com sucesso!");
+    }
+
+    public static void ExcluirLivro(List<Livro> livros)
+    {
+        Console.WriteLine("Digite o título do livro que deseja excluir: ");
+        string tituloBusca = Console.ReadLine();
+
+        Livro livroEncontrado = null;
+
+        foreach (Livro li in livros)
+        {
+            if (li.Titulo.Equals(tituloBusca, StringComparison.OrdinalIgnoreCase))
+            {
+                livroEncontrado = li;
+                break;
+            }
+        }
+
+        if (livroEncontrado == null)
+        {
+            Console.WriteLine("Livro não encontrado na biblioteca.");
+            return;
+        }
+
+        livros.Remove(livroEncontrado);
+
+        Console.WriteLine($"O livro '{livroEncontrado.Titulo}' foi excluído do sistema.");
+    }
+
+    public static void ListarLirvosDisponiveis(List<Livro> livros)
+    {
+        bool existeLivroDisponivel = false;
+
+                foreach (Livro livro in livros){
+                    if(livro.Disponivel){
+                        livro.Exibir();
+                        existeLivroDisponivel = true;
+                    }
+                }
+                if(!existeLivroDisponivel == false){
+                    Console.WriteLine("Nenhum livro disponível no momento.");
+                }
+    }
+
+    public static void ListarLirvosEmprestados(List<Livro> livros)
+    {
+        bool existeLivroEmprestado = false;
+
+                foreach (Livro livro in livros){
+                    if(!livro.Disponivel){
+                        livro.Exibir();
+                        existeLivroEmprestado = true;
+                    }
+                }
+                if(!existeLivroEmprestado == false){
+                    Console.WriteLine("Nenhum livro emprestado no momento.");
+                }
+    }
+
+    public static void RealizarEmprestimo(List<Livro> livros, List<Usuario> usuarios, List<Emprestimo> emprestimos)
+    {
+      Console.WriteLine("Digite o nome do usuário que deseja realizar o empréstimo: ");
+      string nomeUsuario = Console.ReadLine();
+        Usuario usuarioEncontrado = null;
+        foreach (Usuario usuario in usuarios)
+        {
+            if (usuario.Nome.Equals(nomeUsuario, StringComparison.OrdinalIgnoreCase))
+            {
+                usuarioEncontrado = usuario;
+                break;
+            }
+        }
+        if (usuarioEncontrado == null)
+        {
+            Console.WriteLine("Usuário não encontrado.");
+            return;
+        }
+
+        Console.WriteLine("Digite o título do livro que deseja emprestar: ");
+        string tituloLivro = Console.ReadLine();
+        Livro livroEncontrado = null;
+        foreach (Livro livro in livros)
+        {
+            if (livro.Titulo.Equals(tituloLivro, StringComparison.OrdinalIgnoreCase))
+            {
+                livroEncontrado = livro;
+                break;
+            
+            }
+        }
+        if (livroEncontrado == null)
+        {
+            Console.WriteLine("Livro não encontrado.");
+            return;
+        }
+        if (!livroEncontrado.Disponivel)
+        {
+            Console.WriteLine("Livro indisponível para empréstimo.");
+            return;
+        }
+        Console.WriteLine("Digite a quantidade de dias para o empréstimo: ");
+        int quantidadeDias = int.Parse(Console.ReadLine());
+        Emprestimo emprestimo = new Emprestimo(livroEncontrado, usuarioEncontrado, quantidadeDias);
+        emprestimos.Add(emprestimo);
+        livroEncontrado.Disponivel = false;
+        Console.WriteLine("Empréstimo realizado com sucesso!");
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@ List<Livro> livros = new List<Livro>();
 List<Usuario> usuarios = new List<Usuario>();
 List<Emprestimo> emprestimos = new List<Emprestimo>();
 
+usuarios.Add(new Usuario("João Silva", 28, "11999999999"));
+usuarios.Add(new Usuario("Maria Santos", 35, "11988888888"));
+usuarios.Add(new Usuario("Pedro Oliveira", 22, "11977777777"));
+
+livros.Add(new Livro("Clean Code", "Robert Martin", 2008));
+livros.Add(new Livro("Design Patterns", "Gang of Four", 1994));
+livros.Add(new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954));
+livros.Add(new Livro("1984", "George Orwell", 1949));
+
 int escolha = 0;
 
 do
@@ -16,8 +25,8 @@ do
     Console.WriteLine("4 - Listar Todos os Livros");
     Console.WriteLine("5 - Buscar Livro");
     Console.WriteLine("6 - Excluir Livro");
-    Console.WriteLine("7 - Consultar Disponibilidade");
-    Console.WriteLine("8 - Consultar Indisponiveis");
+    Console.WriteLine("7 - Consultar Livros Disponíveis");
+    Console.WriteLine("8 - Consultar Livros Emprestados");
     Console.WriteLine("9 - Realizar Empréstimo");
     Console.WriteLine("10 - Devolver Livro");
     Console.WriteLine("0 - Sair");
@@ -26,11 +35,27 @@ do
 
     switch (escolha)
     {
-        case 1: ;                              
+        case 1: Biblioteca.CadastrarUsuario(usuarios);                              
         break;
-        case 2: ;                              
+        case 2: Biblioteca.ListarUsuarios(usuarios);                              
         break;
         case 3: Biblioteca.CadastrarLivro(livros);                              
+        break;
+        case 4: Biblioteca.ListarLivros(livros);                              
+        break;
+        case 5: Biblioteca.BuscarLivro(livros);                         
+        break;
+        case 6: Biblioteca.ExcluirLivro(livros);                              
+        break;
+        case 7: Biblioteca.ListarLirvosDisponiveis(livros);                             
+        break;
+        case 8: Biblioteca.ListarLirvosEmprestados(livros);                             
+        break;
+        case 9: Biblioteca.RealizarEmprestimo(livros, usuarios, emprestimos);                              
+        break;
+        case 10:                              
+        break;
+        case 0:                              
         break;
         
         default: Console.WriteLine("Opção inválida. Tente novamente.");             
